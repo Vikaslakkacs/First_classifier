7ff85937349c21362d550b7337dbeda21fa9e891
Vikas Lakka <vikaslakkacs@gmail.com>
2023-03-29 17:35:53 +0530

    configuration updated for base model preparation

--- src/firstClassifier/config/configuration.py
+++ src/firstClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from firstClassifier.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from firstClassifier.utils import read_yaml, create_directories
-from firstClassifier.entity import DataIngestionConfig
-
+from firstClassifier.entity import DataIngestionConfig, PrepareBaseModelConfig
+from pathlib import Path
 
 
 class ConfigurationManager:
@@ -41,3 +41,29 @@ class ConfigurationManager:
             unzip_dir= config.unzip_dir
             )
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self)->PrepareBaseModelConfig:
+        """Creates root directory for preparing base model and stores all the variables
+            from config.yaml
+        Returns:
+            PrepareBaseModelConfig: PrepareBaseModelConfig
+        """
+        ##Config contains the attributes that are present in prepare_base_model in config.yaml
+        
+        config= self.config.prepare_base_model
+        params= self.params
+        ##Create directory from root_dir
+        create_directories([config.root_dir])
+        
+
+        prepare_base_model_config= PrepareBaseModelConfig(   
+                    root_dir= Path(config.root_dir),
+                    base_model_path= Path(config.base_model_path),
+                    updated_base_model_path= Path(config.updated_base_model_path),
+                    params_image_size= params.IMAGE_SIZE,
+                    params_learning_rate= params.LEARNING_RATE,
+                    params_include_top= params.INCLUDE_TOP,
+                    params_weights= params.WEIGHTS,
+                    params_classes= params.CLASSES
+            )
+        return prepare_base_model_config
