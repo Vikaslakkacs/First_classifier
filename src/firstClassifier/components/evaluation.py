3030a6cb490305f7d6d3b1273fe3ce569ebb51ee
Vikas Lakka <vikaslakkacs@gmail.com>
2023-04-17 15:32:13 +0530

    addition of mlflow

--- src/firstClassifier/components/evaluation.py
+++ src/firstClassifier/components/evaluation.py
@@ -6,7 +6,9 @@ import time
 from pathlib import Path
 from firstClassifier.utils import save_json
 from firstClassifier.entity import EvaluationConfig
-
+import mlflow
+import mlflow.tensorflow
+from urllib.parse import urlparse
 
 class Evaluation:
     def __init__(self, config: EvaluationConfig):
@@ -18,7 +20,7 @@ class Evaluation:
         ##Augmentation
         datagenerator_kwargs= dict(
             rescale= 1./255,
-            validation_split= 0.30
+            validation_split= 0.005
 
         )
 
@@ -56,3 +58,20 @@ class Evaluation:
         scores= {"loss": self.score[0], "accuracy": self.score[1]}
         save_json(path=Path("scores.json"), data= scores)
 
+    def log_into_mlflow(self):
+        mlflow.set_registry_uri(self.config.mlflow_uri)
+        #print("f{self.config.mlflow_uri}-----Hello")
+        tracking_url_type_store= urlparse(mlflow.get_tracking_uri()).scheme
+
+        with mlflow.start_run():
+
+            mlflow.log_params(
+                self.config.all_params
+            )
+            mlflow.log_metrics(
+                {"loss": self.score[0], "accuracy": self.score[1]}
+            )
+            if tracking_url_type_store!="file":
+                mlflow.keras.log_model(self.model, "model", registered_model_name="VGG16 model")
+            else:
+                mlflow.keras.log_model(self.model, "model")
--- src/firstClassifier/pipeline/stage_04_evaluation.py
+++ src/firstClassifier/pipeline/stage_04_evaluation.py
@@ -1,6 +1,11 @@
 from firstClassifier.components.evaluation import Evaluation
 from firstClassifier.config.configuration import ConfigurationManager
 from firstClassifier import logger
+import os
+##Setting up environment variables for mlflow
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/Vikaslakkacs/First_classifier.mlflow "
+os.environ["MLFLOW_TRACKING_USERNAME"]="Vikaslakkacs"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="7af2883531f922b7f10037acc2db95ccdd02bfa5"
 
 STAGE_NAME="Data Evaluation Stage"
 def main():
@@ -8,7 +13,8 @@ def main():
     validation_config= config.get_validation_config()
     evaluate= Evaluation(validation_config)
     evaluate.evaluation()
-    evaluate.save_score()  
+    evaluate.save_score()
+    evaluate.log_into_mlflow()  
 
 
 if __name__=="__main__":
